9a8a688de296c324bcd47b1536c64b559558bd92
Piotr Świebocki <swpiotr5@interia.pl>
2023-05-23 18:29:51 +0200

    Add files via upload

---
+++ src/main/java/efs.task.oop/ExtraordinaryVillager.java
@@ -0,0 +1,37 @@
+public class ExtraordinaryVillager extends Villager {
+    public enum Skill{
+        IDENTIFY("I will identify items for you at no charge."),
+        SHELTER("I can offer you poor shelter.");
+
+        private String villagerInfo;
+
+        Skill(String villagerInfo) {
+            this.villagerInfo = villagerInfo;
+        }
+
+        public String getVillagerInfo() {
+            return villagerInfo;
+        }
+    }
+
+    private Skill skill;
+
+    public ExtraordinaryVillager(String name, int age, Skill skill) {
+        super(name, age);  // Wywołanie konstruktora klasy nadrzędnej Villager
+        this.skill = skill;
+    }
+
+    @Override
+    public void sayHello(){
+        System.out.println("Greetings traveler... I'm " + getName() + " and I'm " + getAge() + " years old. " + skill.getVillagerInfo());
+    }
+
+    @Override
+    public void attack(Fighter victim) {
+    }
+
+    @Override
+    public void takeHit(int damage) {
+        setHealth(0);
+    }
+}
